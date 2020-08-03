1709145dad21648884a95217e8c0daafc72a5c51
Caio Sena <caio@prodam.am.gov.br>
2020-08-03 18:19:00 -0400

    Ao informar os produtos é retornado os produtos em promoção, se houver; Ao digitar a palavra 'todos', todos os produtos cadastrados são exibidos;

--- src/main/java/br/com/carrefour/despensacarrefour/bot/Bot.java
+++ src/main/java/br/com/carrefour/despensacarrefour/bot/Bot.java
@@ -1,7 +1,9 @@
 package br.com.carrefour.despensacarrefour.bot;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.inject.Singleton;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -10,6 +12,11 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -22,6 +29,9 @@ public class Bot extends TelegramLongPollingBot{
 	
 	@Value("${bot.username}")
 	private String username;
+	
+	@Singleton
+	ObjectMapper mapper = new ObjectMapper();
 
 	@Override
 	public void onUpdateReceived(Update update) {
@@ -47,34 +57,47 @@ public class Bot extends TelegramLongPollingBot{
 		response.setChatId(chatId);
 		String text = null;
 		
-		if(mensagem.isCommand() && mensagem.getText().equals("/start")) {
-			StringBuilder sb = new StringBuilder();
-			sb.append("Bem Vindo ao Bot Despensa Carrefour! Aqui você pode informar os produtos que estão faltando na sua despensa e mostraremos se existem promoções para os produtos.");
-			sb.append("\nFácil assim. Agora me diz, quais produtos estão faltando na sua despensa? Escreva os produtos separados por vígula.");
-			text = sb.toString();
-		} else {
-			List<String> listaProdutosCliente = List.of(mensagem.getText().split(","));
-			List<Produto> listaProdutosEmPromocao = getListaProdutosPromocao(listaProdutosCliente);
-			
-			text = listaProdutosEmPromocao.toString();
-		}
-		
 		try {
+			if(mensagem.isCommand() && mensagem.getText().trim().equalsIgnoreCase("/start")) {
+				StringBuilder sb = new StringBuilder();
+				sb.append("Bem Vindo ao Bot Despensa Carrefour! Aqui você pode informar os produtos que estão faltando na sua despensa e mostraremos se existem promoções para os produtos.");
+				sb.append("\nFácil assim. Agora me diz, quais produtos estão faltando na sua despensa? Escreva os produtos separados por vígula. \n(Para exibir todos os produtos em promoção, escreva 'todos')");
+				text = sb.toString();
+			} else if(mensagem.getText().trim().equalsIgnoreCase("todos")) {
+				text = getListaTodosProdutos().toString();
+			} else {
+				List<String> listaProdutosCliente = List.of(mensagem.getText().split(","));
+				List<Produto> listaProdutosEmPromocao = getListaProdutosPromocao(listaProdutosCliente);
+				
+				if(listaProdutosEmPromocao.isEmpty())
+					text = "Infelizmente os produtos que você informou não estão em promoção";
+				else
+					text = listaProdutosEmPromocao.toString();
+			}
+			
 			response.setText(text);
 			execute(response);
-			log.info("Sent message \"{}\" to {}", text, chatId);
+			log.info("Mensagem enviada \"{}\" to {}", text, chatId);
 		} catch (TelegramApiException e) {
-			log.error("Failed to send message \"{}\" to {} due to error: {}", text, chatId, e.getMessage());
+			log.error("Falha ao enviar a mensagem \"{}\" para {} acontece o erro: {}", text, chatId, e.getMessage());
+		} catch (JsonMappingException e) {
+			log.error("Falha ao enviar a mensagem \"{}\" para {} acontece o erro: {}", text, chatId, e.getMessage());
+		} catch (JsonProcessingException e) {
+			log.error("Falha ao enviar a mensagem \"{}\" para {} acontece o erro: {}", text, chatId, e.getMessage());
 		}
 	}
 	
-	public List<Produto> getListaProdutosPromocao(List<String> listaProdutosCliente){
-		List<Produto> listaProdutosEmPromocao = new ArrayList<Produto>();
-		for (String nome : listaProdutosCliente) {
-			Produto produto = new Produto(nome, "R$ 50,00");
-			listaProdutosEmPromocao.add(produto);
-		}
-		return listaProdutosEmPromocao;
+	public List<Produto> getListaProdutosPromocao(List<String> listaProdutosCliente) throws JsonMappingException, JsonProcessingException{
+		List<Produto> listaTodosProdutos = getListaTodosProdutos();
+		
+		return listaTodosProdutos.stream()
+				.filter((produto) -> listaProdutosCliente.stream()
+						.anyMatch(produtoCliente -> produtoCliente.trim().equalsIgnoreCase(produto.getNome().trim())))
+				.collect(Collectors.toList());
+	}
+	
+	public List<Produto> getListaTodosProdutos() throws JsonMappingException, JsonProcessingException{
+		return mapper.readValue(Produto.listaTodosProdutos().toString(), new TypeReference<List<Produto>>(){});
 	}
 
 }
--- src/main/java/br/com/carrefour/despensacarrefour/bot/Produto.java
+++ src/main/java/br/com/carrefour/despensacarrefour/bot/Produto.java
@@ -13,15 +13,25 @@ public class Produto implements Serializable{
 	
 	private String nome;
 	private String preco;
+	private String unidadeMedida;
+	private boolean promocao;
 	
-	public Produto(String nome, String preco) {
-		this.nome = nome;
-		this.preco = preco;
-	}
-
 	@Override
 	public String toString() {
-		return "{nome: "+nome+", valor: "+preco+"}";
+		return nome + " - " + preco;
+	}
+	
+	public static String listaTodosProdutos(){
+		return "[ {\"nome\" : \"banana\", \"preco\": \"R$ 3,00\", \"unidadeMedida\": \"kg\", \"promocao\": true} ,"
+				+ "{\"nome\" : \"tomate\", \"preco\": \"R$ 5,50\", \"unidadeMedida\": \"kg\", \"promocao\": false} ,"
+				+ "{\"nome\" : \"cebola\", \"preco\": \"R$ 4,20\", \"unidadeMedida\": \"kg\", \"promocao\": false} ,"
+				+ "{\"nome\" : \"pneu\", \"preco\": \"R$ 150,00\", \"unidadeMedida\": \"unidade\", \"promocao\": true} ,"
+				+ "{\"nome\" : \"queijo\", \"preco\": \"R$ 7,00\", \"unidadeMedida\": \"kg\", \"promocao\": false} ,"
+				+ "{\"nome\" : \"presunto\", \"preco\": \"R$ 5,00\", \"unidadeMedida\": \"kg\", \"promocao\": true} ,"
+				+ "{\"nome\" : \"alvejante\", \"preco\": \"R$ 16,00\", \"unidadeMedida\": \"ml\", \"promocao\": false} ,"
+				+ "{\"nome\" : \"cotonete\", \"preco\": \"R$ 4,80\", \"unidadeMedida\": \"g\", \"promocao\": false} ,"
+				+ "{\"nome\" : \"oregano\", \"preco\": \"R$ 0,90\", \"unidadeMedida\": \"mg\", \"promocao\": false} ,"
+				+ "{\"nome\" : \"pilha\", \"preco\": \"R$ 7,80\", \"unidadeMedida\": \"unidade\", \"promocao\": false} ]";
 	}
 	
 }
